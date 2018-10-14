c76cb4b38b0dfe7ab9bb603b90c80c93d4bdd295
Jaly Nikkolai Santamaría Gonzalez <jalysgbss@gmail.com>
2018-10-14 13:05:44 -0500

    Recuperación

---
+++ JalySantamaria-TableTennis/Assets/Scripts/RightPaddleController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightPaddleController : MonoBehaviour {
+
+    public float MotionSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+
+            //Esta instrucción mueve el GameObject hacia su izquierda        
+            this.transform.Translate(Vector3.left * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+
+            //Esta instrucción mueve el GameObject hacia su derecha       
+            this.transform.Translate(Vector3.right * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+
+            this.transform.Translate(Vector3.up * MotionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+
+            this.transform.Translate(Vector3.down * MotionSpeed);
+        }
+
+    }
+}
